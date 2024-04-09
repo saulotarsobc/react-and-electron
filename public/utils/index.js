c54ea81f0a7400a1ff7c4f9632bc312d35e3c1c6
saulotarsobc <saulotarsobc@gmail.com>
2024-04-09 01:15:30 -0300

    :fire:

--- public/utils/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.databaseUrl = void 0;
-const electron_1 = require("electron");
-const path_1 = require("path");
-exports.databaseUrl = "file:" + (0, path_1.join)(electron_1.app.getPath("userData"), "database.sqlite");
